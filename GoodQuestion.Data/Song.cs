1534be2792bbed8dcd2a30309d30870b28e1c667
Kyle Davison <kyle.w.davison@gmail.com>
2019-08-02 11:18:30 -0400

    Created the Data class for Songs.

---
+++ GoodQuestion.Data/Song.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodQuestion.Data
+{
+    public class Song
+    {
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public string Artists { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string PlayerUrl { get; set; }
+
+        public string Href { get; set; }
+
+        public int DurationMs { get; set; }
+
+        public bool HasAudioFeatures { get; set; }
+
+        //public virtual ICollection<Playlist> Playlists { get; set; }
+
+        public DateTime LastRefreshed { get; set; }
+
+        public float Danceability { get; set; }
+
+        public float Energy { get; set; }
+
+        public int Key { get; set; }
+
+        public float Loudness { get; set; }
+
+        public int Mode { get; set; }
+
+        public float Speechiness { get; set; }
+
+        public float Acousticness { get; set; }
+
+        public float Instrumentalness { get; set; }
+
+        public float Liveness { get; set; }
+
+        public float Valence { get; set; }
+
+        public float Tempo { get; set; }
+
+    }
+}
