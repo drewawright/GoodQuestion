12ec30e01fffb73bc2d9245d61090a188a81711b
Kyle Davison <kyle.w.davison@gmail.com>
2019-08-05 09:49:27 -0400

    Creatd the various data models for Songs

---
+++ GoodQuestion.Models/Song/SongEdit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodQuestion.Models.Song
+{
+    public class SongEdit
+    {
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        //public virtual ICollection<Playlist> Playlists { get; set; }
+    }
+}
