05c081c7100f33dc5a1b5d74b83f469efed1c927
drewawright <drewawright@gmail.com>
2019-08-05 11:52:15 -0400

    Account Models modification
    Added properties to account binding and view models to match IdentityModel
    
    Created Account Detail Model
    
    Updated Scopes in authentication options

---
+++ GoodQuestion.WebAPI/Models/AccountDetailModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoodQuestion.WebAPI.Models
+{
+    public class AccountDetailModel
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public bool HasPlaylists { get; set; }
+        public float Danceability { get; set; }
+        public float Energy { get; set; }
+        public int Key { get; set; }
+        public float Loudness { get; set; }
+        public int Mode { get; set; }
+        public float Speechiness { get; set; }
+        public float Acousticness { get; set; }
+        public float Instrumentalness { get; set; }
+        public float Liveness { get; set; }
+        public float Valence { get; set; }
+        public float Tempo { get; set; }
+        public int Duration_ms { get; set; }
+    }
+}
