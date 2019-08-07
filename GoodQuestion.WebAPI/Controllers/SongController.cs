8188cf823d9d463095b2e2461c299c0578824b9f
Kyle Davison <kyle.w.davison@gmail.com>
2019-08-07 10:56:51 -0400

    created song service

--- GoodQuestion.WebAPI/Controllers/SongController.cs
+++ GoodQuestion.WebAPI/Controllers/SongController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoodQuestion.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,11 @@ namespace GoodQuestion.WebAPI.Controllers
 {
     public class SongController : ApiController
     {
+
+        private SongServices CreateSongServices()
+        {
+            var songService = new SongServices();
+            return songService;
+        } 
     }
 }
