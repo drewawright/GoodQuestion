e39706ba6023c7a97a41f03ee309499e39570f13
drewawright <drewawright@gmail.com>
2019-08-08 13:56:40 -0400

    added album property to songs
    made db migrations

--- GoodQuestion.Models/Song/SongIndex.cs
+++ GoodQuestion.Models/Song/SongIndex.cs
@@ -13,6 +13,7 @@ namespace GoodQuestion.Models.Song
         public string SongId { get; set; }
 
         public string Artists { get; set; }
+        public string AlbumName { get; set; }
 
         public string ImageUrl { get; set; }
 
