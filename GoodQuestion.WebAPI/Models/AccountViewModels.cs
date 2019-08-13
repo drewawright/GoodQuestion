ada64775de26003e888244abda3d06d2328c5a90
Kyle Davison <kyle.w.davison@gmail.com>
2019-08-13 11:13:37 -0400

    Changed Email to UserName
    does not change functionality, just renaming the property to be in line with its actual value.

--- GoodQuestion.WebAPI/Models/AccountViewModels.cs
+++ GoodQuestion.WebAPI/Models/AccountViewModels.cs
@@ -28,11 +28,13 @@ namespace GoodQuestion.WebAPI.Models
 
     public class UserInfoViewModel
     {
-        public string Email { get; set; }
+        public string UserName { get; set; }
 
         public bool HasRegistered { get; set; }
 
         public string LoginProvider { get; set; }
+
+        public string SpotifyUserId { get; set; }
     }
 
     public class UserLoginInfoViewModel
